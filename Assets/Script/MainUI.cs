12518218a6b722aa92569101e3453fa6a0f4f78a
DESKTOP-QVOJ483\iibi203 <mnbvcxz195@naver.com>
2023-01-17 11:50:41 +0900

    Main UI 수정

--- Assets/Script/MainUI.cs
+++ Assets/Script/MainUI.cs
@@ -7,8 +7,7 @@ public class MainUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("용헌이바보");
-        Debug.Log("용준이바보");
+
     }
 
     // Update is called once per frame
